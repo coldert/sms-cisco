c125d56179930443822f5081a4ab5489fca4bd03
Robert Wictorzon <robert@wictorzon.com>
2015-02-13 10:16:14 +0100

    Regexp for interface now handles s0/0/0 and similar identifiers

--- sms_tolk2.py
+++ sms_tolk2.py
@@ -2,7 +2,10 @@
 #Python 2.7.3, Script that matches a string against XML document.
 
 #Test command to match XML document.
-cmdstring = "int port-channel0/12 ip 192.168.1.1/29"
+#cmdstring = "int port-channel0/12 ip 192.168.1.1/29"
+#cmdstring = "int f0/12 down"
+#cmdstring = "route static 209.128.3.1/24 s0/1/0"
+cmdstring = "route default s0/1/0"
 #Makes string lowercase and splits it of at space,enter,tab etc..
 cmd_list = cmdstring.lower().split()
 
@@ -12,7 +15,7 @@ import sys
 
 #Define patterns for IP ADDRESS, INTERFACE ...
 re_ipaddress = re.compile("^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/?(\d{1,2})?$")
-re_interface = re.compile("^([a-z-]+\d{1,2}/\d{1,2})$")
+re_interface = re.compile("^([a-z-]+\d{1,2}(/\d{1,2})*?)$")
 
 #Parse the XML file to tree.
 tree = ET.parse('cmdtable2.xml')
@@ -21,24 +24,32 @@ root = tree.getroot()
 #Function to convert /24 to 255.255.255.0
 #code.activestate.com/recipies/576483-convert-subnetmask-from-cidr-notation-to-dotdecima/
 def cidrToDotted(mask):
-	bits = 0
-	for i in xrange(32-mask,32):
-		bits |= (1<<i)
-	return "%d.%d.%d.%d" % ((bits & 0xff000000) >> 24, (bits & 0xff0000) >> 16, (bits & 0xff00) >> 8, (bits & 0xff))
+	if mask == "":
+		return ""
+	else:
+		mask = int(mask)
+		bits = 0
+		for i in xrange(32-mask,32):
+			bits |= (1<<i)
+		return "%d.%d.%d.%d" % ((bits & 0xff000000) >> 24, (bits & 0xff0000) >> 16, (bits & 0xff00) >> 8, (bits & 0xff))
 
 def buildCommand(cmd):
 	cmd = cmd.replace("#INTERFACE", str_interface)	
 	cmd = cmd.replace("#IPADDRESS", str_ip)
-	cmd = cmd.replace("#MASK", cidrToDotted(int(str_mask)))	
+	cmd = cmd.replace("#MASK", cidrToDotted(str_mask))	
 	return cmd
 
 #Build a search string for etree iterfind function
-xpath_str = "" 
+xpath_str = ""
+str_ip = ""
+str_mask = ""
+str_interface = ""
 for i in range(len(cmd_list)):
 	#Check if current cmd is ip address or interface name
 	match_ip = re_ipaddress.match(cmd_list[i])
 	match_int = re_interface.match(cmd_list[i])
 	if match_ip:
+		#TODO: Handle multiple instances of ip addresses in a command
 		str_ip = match_ip.group(1)
 		str_mask = match_ip.group(2)
 	elif match_int:
@@ -56,6 +67,6 @@ for cmd in root.iterfind(xpath_str):
 	print "\nCOMMAND: " + buildCommand(cmd.text) + "\n"
 
 #The matched IP address and interface name, if available
-#if str_ip: print "IP ADDRESS: " + str_ip
-#if str_mask: print "SUBNET MASK: " + cidrToDotted(int(str_mask))
-#if str_interface: print "INTERFACE: " + str_interface
+if str_ip: print "IP ADDRESS: " + str_ip
+if str_mask: print "SUBNET MASK: " + cidrToDotted(str_mask)
+if str_interface: print "INTERFACE: " + str_interface
