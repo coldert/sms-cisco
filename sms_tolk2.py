84f2df3e5a99c9fcd35d7c4b6b9caa675c09a8f0
Robert Wictorzon <robert@wictorzon.com>
2015-02-16 21:45:25 +0100

    Support multiple ip-addresses.

--- sms_tolk2.py
+++ sms_tolk2.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python
 # Python 2.7.3 script that matches a string against XML document.
 
-#TODO: Classify. Make a command object.
-
 import xml.etree.ElementTree as ET
 import re
 import sys
 
 # Test commands
 #cmd_string = "int port-channel0/12 ip 192.168.1.1/29"
-#cmd_string = "int f0/12 down"
-#cmd_string = "route static 209.128.3.1/24 s0/1/0"
-cmd_string = "route default s0/1/0"
+cmd_string = "int f0/12 down"
+#cmd_string = "route static 209.128.3.0/24 s0/1/0 209.128.3.3"
+#cmd_string = "route default s0/1/0"
+
 # Makes string lowercase and splits it of at space,enter,tab etc..
 cmd_list = cmd_string.lower().split()
 
-# Define patterns for IP address, interface identifier ...
+# Define patterns for IP address, interface placeholder ...
 re_ipaddress = re.compile("^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/?(\d{1,2})?$")
 re_interface = re.compile("^([a-z-]+\d{1,2}(/\d{1,2})*?)$")
+re_placeholder = re.compile(r"#.*\b")
 
 # Parse the XML file to tree.
 tree = ET.parse('cmdtable2.xml')
@@ -37,28 +37,30 @@ def cidrToDotted(mask):
 
 # Build the actual command to be sent to the device
 def buildCommand(cmd):
-	cmd = cmd.replace("#INTERFACE", str_interface)	
-	cmd = cmd.replace("#IPADDRESS", str_ip)
-	cmd = cmd.replace("#MASK", cidrToDotted(str_mask))	
+	if len(str_ip) > 0: # Replace first (only?) ip-address (i.e interface address, destination network in static route)
+		cmd = cmd.replace("#IPADDRESS", str_ip[0], 1)
+	if len(str_ip) > 1: # Replace second ip-address (i.e the next hop in a static route)
+		cmd = cmd.replace("#IPADDRESS", str_ip[1], 1)
+	if str_interface: cmd = cmd.replace("#INTERFACE", str_interface[0])	
+	# TODO: Remove all unused placeholders
+	cmd = re_placeholder.sub("", cmd)
 	return cmd
 
 # Build a search string for etree iterfind function
 xpath_str = ""
 # Variables to hold IP address, subnet mask and interface identifier
-str_ip = ""
-str_mask = ""
-str_interface = ""
+str_ip = []
+str_interface = []
 
 for i in range(len(cmd_list)):
 	# Check if current cmd is an IP address or an interface identifier
 	match_ip = re_ipaddress.match(cmd_list[i])
 	match_int = re_interface.match(cmd_list[i])
 	if match_ip:
-		# TODO: Handle multiple instances of IP addresses in a command
-		str_ip = match_ip.group(1)
-		str_mask = match_ip.group(2)
+		str_mask = " " + cidrToDotted(match_ip.group(2)) if (match_ip.group(2)) else "";
+		str_ip.append(match_ip.group(1) + str_mask)
 	elif match_int:
-		str_interface = match_int.group(1)
+		str_interface.append(match_int.group(1))
 	else:
 		# Build each level of search based on number of items in cmd_list
 		xpath_str += cmd_list[i] + "/" 
@@ -70,8 +72,3 @@ xpath_str = xpath_str[:-1]
 for cmd in root.iterfind(xpath_str):
 	# Print the text inside the xml tag (i.e the actual command to be sent to the Cisco device
 	print "\nCOMMAND: " + buildCommand(cmd.text) + "\n"
-
-# Print the matched IP address and interface identifier
-if str_ip: print "IP ADDRESS: " + str_ip
-if str_mask: print "SUBNET MASK: " + cidrToDotted(str_mask)
-if str_interface: print "INTERFACE: " + str_interface
