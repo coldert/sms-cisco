e11158e693a6c29afadb2a92e5deb362a90d8a37
Robert Wictorzon <robert@wictorzon.com>
2015-02-17 23:05:00 +0100

    Cleaning up.

--- get_sms.py
+++ get_sms.py
@@ -1,24 +1,28 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from urlparse import urlparse
+from urllib import unquote
+import sms_tolk2 as SMS
 
 PORT_NUMBER = 8080
 
-class myRequestHandler(BaseHTTPRequestHandler):
-  def do_GET(self):
-    parsed_path = urlparse(self.path)
-    query_array = parsed_path.query.split('&')
-	#Call command parser
-	#SSH to router
-    self.send_response(200)
-    self.send_header('Content-type','text/html')
-    self.end_headers()
-    self.wfile.write(query_array)
-    return
+class ReceivedSmsHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		command = ""
+		parsed_path = urlparse(self.path)
+		query_list = parsed_path.query.split('&')
+		for i in query_list:
+			part = i.split('=')
+			if part[0] == 'text': command = unquote(part[1])
+		self.send_response(200)
+		self.send_header('Content-type','text/html')
+		self.end_headers()
+		self.wfile.write(SMS.parse(command))
+		return
 try:
-  server = HTTPServer(('', PORT_NUMBER), myRequestHandler)
-  print 'Started httpserver on port ', PORT_NUMBER, '. Ctrl-C to stop.'
-  server.serve_forever()
+	server = HTTPServer(('', PORT_NUMBER), ReceivedSmsHandler)
+	print 'Started httpserver on port ', PORT_NUMBER, '. Ctrl-C to stop.'
+	server.serve_forever()
 except KeyboardInterrupt:
-  print 'Shutting down the web server...'
-  server.socket.close()
+	print 'Shutting down the web server...'
+	server.socket.close()
 
--- sms_tolk2.py
+++ sms_tolk2.py
@@ -15,24 +15,29 @@ re_interface = re.compile("^([a-z-]+\d{1,2}(/\d{1,2})*?)$")
 re_ad = re.compile("^ad(\d*)$")
 re_placeholder = re.compile(r"#.*\b")
 
-# Variables to hold IP address, subnet mask and interface identifier
-str_ip = []
-str_interface = []
-str_ad = []
-
-def command_handler(cmd):
-	# Makes string lowercase and splits it of at space,enter,tab etc..
+# MAIN FUNCTION
+# This function is called by the request handler when an sms is received
+def parse(cmd_string):
+	# Make string lowercase and split into command parts
 	cmd_list = cmd_string.lower().split()
 
 	# Build a search string for etree iterfind function
 	xpath_str = ""
+	# Lists to hold IP address, interface identifier and other user defined values
+	str_ip = []
+	str_interface = []
+	str_ad = []
 
-	for i in range(len(cmd_list)):
+	# Build the xpath search string from the command parts, but store ip addresses, 
+	# interface identifiers and other user defined values for later
+	for cmd in cmd_list:
 		# Check if current cmd is an IP address or an interface identifier
-		match_ip = re_ipaddress.match(cmd_list[i])
-		match_int = re_interface.match(cmd_list[i])
-		match_ad = re_ad.match(cmd_list[i])
+		match_ip = re_ipaddress.match(cmd)
+		match_int = re_interface.match(cmd)
+		match_ad = re_ad.match(cmd)
+
 		if match_ip:
+			# Check if the ip address should have a subnet mask
 			str_mask = " " + cidr_to_dotted(int(match_ip.group(2))) if (match_ip.group(2)) else "";
 			str_ip.append(match_ip.group(1) + str_mask)
 		elif match_int:
@@ -40,24 +45,28 @@ def command_handler(cmd):
 		elif match_ad:
 			str_ad.append(match_ad.group(1))
 		else:
-			# Build each level of search based on number of items in cmd_list
-			xpath_str += cmd_list[i] + "/" 
+			# Build each level of xpath search from non user defined parts of the command string
+			xpath_str += cmd + "/" 
 
 	# Remove the last / from the search string
 	xpath_str = xpath_str[:-1]
 
-	# Find XML nodes that match the search string and print the command
-	for cmd in root.iterfind(xpath_str):
-		command = cmd.text
-		# Loop through all ip-addresses and interfaces and put the in the right place in the command
-		for i in range(len(str_ip)):
-			command = command.replace("#IPADDRESS", str_ip[i], 1)
-		for i in range(len(str_interface)):
-			command = command.replace("#INTERFACE", str_interface[i], 1)
-		for i in range(len(str_ad)):
-			command = command.replace("#AD", str_ad[i], 1)	
-		# Remove all unused placeholders
-		command = re_placeholder.sub("", command)
+	# Find XML nodes that match the search string and replace the placeholders with their correct value
+	command = ""
+	try:
+		for cmd in root.iterfind(xpath_str):
+			command = cmd.text
+			# Loop through all ip-addresses and interfaces and put the in the right place in the command
+			for i in str_ip:
+				command = command.replace("#IPADDRESS", i, 1)
+			for i in str_interface:
+				command = command.replace("#INTERFACE", i, 1)
+			for i in str_ad:
+				command = command.replace("#AD", i, 1)	
+			# Remove all unused placeholders
+			command = re_placeholder.sub("", command)
+	except StopIteration:
+		pass
 	return command
 
 # Function to convert /24 to 255.255.255.0
@@ -73,4 +82,4 @@ def cidr_to_dotted(mask):
 #cmd_string = "int f0/12 down"
 #cmd_string = "route static 209.128.3.0/24 s0/1/0 209.128.3.3 ad150"
 #cmd_string = "route default s0/1/0 192.168.24.254"
-#print command_handler(cmd_string)
+#print parse(cmd_string)
